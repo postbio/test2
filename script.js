5161405c88245cc38cde267a408a4ab13b4295f2
postbio <kevin.ann@gmail.com>
2023-12-17 00:59:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,5 @@
-const bird = document.getElementById('bird');
-let birdY = 200;
-let gravity = 2;
-let jump = -30;
-let gameRunning = true;
-
-function gameLoop() {
-    if (gameRunning) {
-        birdY += gravity;
-        bird.style.top = birdY + 'px';
-
-        if (birdY >= window.innerHeight || birdY <= 0) {
-            gameOver();
-        }
-
-        requestAnimationFrame(gameLoop);
-    }
+function changePattern(designNumber) {
+    const kaleidoscope = document.getElementById('kaleidoscope');
+    kaleidoscope.className = ''; // Clear existing classes
+    kaleidoscope.classList.add('design-' + designNumber);
 }
-
-function jumpBird() {
-    birdY += jump;
-}
-
-document.addEventListener('keydown', function (event) {
-    if (event.code === 'Space') {
-        jumpBird();
-    }
-});
-
-function gameOver() {
-    gameRunning = false;
-    alert('Game Over!');
-}
-
-gameLoop();
